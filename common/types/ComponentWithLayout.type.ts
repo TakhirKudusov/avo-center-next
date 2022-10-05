f171d7958aad2bed7d21b36da9c4f42e9e23c370
TakhirKudusov <tahirkudusov1@outlook.com>
2022-10-05 20:17:07 +0500

    feature nft started 0.0

--- common/types/ComponentWithLayout.type.ts
+++ common/types/ComponentWithLayout.type.ts
@@ -2,7 +2,7 @@ import {AppProps} from "next/app";
 import React from "react";
 
 type ComponentWithLayout = AppProps["Component"] & {
-    PageLayout?: React.FC<any>;
+    PageLayout?: React.FC<any>
 };
 
 export type {ComponentWithLayout}
